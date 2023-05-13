24686fb7ea3f80cced1b61e37ef6de7529c9f934
Jose Matias <joseam1789@gmail.com>
2023-05-13 19:21:02 -0400

    Arreglando situaciones en js

--- js/app.js
+++ js/app.js
@@ -1,20 +1,14 @@
 function encriptado(){
-   // alert("Estoy encripando")
     var textoEntrada = document.querySelector("#text").value;
     var textoCifrado = textoEntrada.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
     document.querySelector("#salida").value = textoCifrado;
     document.querySelector("#text").value;
-   /// console.log(textoCifrado)
    document.getElementById("salida").innerHTML = textoCifrado
 }
 
 function desencriptado(){
-    //alert("Estoy desencriptando")
     var textoEntrada = document.querySelector("#text").value;
-    var textoCifrado = textoEntrada.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u");
-    //document.querySelector("salida").value = textoCifrado;
-    //document.querySelector("#text").value;
-    document.getElementById("salida").innerHTML = textoCifrado
-
+    var textoCifrado2 = textoEntrada.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u");
+    document.getElementById("salida").innerHTML = textoCifrado2;
 }
 
